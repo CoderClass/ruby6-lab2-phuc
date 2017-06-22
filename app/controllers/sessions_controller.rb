9dfd9b40bd977d6773325240a0dcb29b6a0b25ef
phuctran <thienphucst92@gmail.com>
2017-06-22 23:30:04 +0700

    Add user logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
   def create
     if @user = User.find_by(email: params[:email])
       if @user.authenticate(params[:password])
+        session[:user_id] = @user.id
         redirect_to root_path
       else
         render 'new'
@@ -15,7 +16,8 @@ class SessionsController < ApplicationController
     end
   end
 
-  def deny_user
-
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   resources :users
-  resources :sessions, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
 
   get 'login' => 'sessions#new'
+  delete 'logout' => 'sessions#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
