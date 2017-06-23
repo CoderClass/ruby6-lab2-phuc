ef5efd41a0ea0b0fcd6d16b9f801fe98fcdb61ac
phuctran <thienphucst92@gmail.com>
2017-06-23 09:58:30 +0700

    Require login to browse profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,28 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :current_user
+  before_action :require_login, only: [:index]
+  before_action :skip_if_logged_in, only: [:new, :create]
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
 
   def signed_in?
-    return session[:user_id].nil?
+    return !session[:user_id].nil?
   end
+
+  private
+    def require_login
+      unless signed_in?
+        flash[:error] = "You must be sign in to view this page!"
+        redirect_to login_path
+      end
+    end
+
+    def skip_if_logged_in
+      if signed_in?
+        redirect_to users_path
+      end
+    end
 end
